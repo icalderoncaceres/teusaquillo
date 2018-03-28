7d80125fa1a5978ea699aca6bfb4c20ad99bc834
Ivan Calderon <ivancalderon@keos.co>
2018-03-28 10:38:40 -0500

    Se agrego la maqueta de la tabla que muestra las reservaciones

--- views/resumen.php
+++ views/resumen.php
@@ -1,4 +1,8 @@
-
+<?php
+	include_once('../clases/bd.php');
+	$bd=new bd();
+	$reservaciones=$bd->doFullSelect('reservaciones');
+?>
         <!-- page content -->
         <div class="right_col" role="main">
         	Resumen
