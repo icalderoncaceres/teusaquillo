523ea22155d1d1fc8c77e2a17954781dbb89df02
ivan calderon <ivandario2010@gmail.com>
2018-05-02 23:07:36 -0500

    Maquetacion del plan de pagos con tablas

--- views/planPagos.php
+++ views/planPagos.php
@@ -1,6 +1,210 @@
+<style>
+	#plan-pagos-table{
+		font-size:14px;
+		font-weight:bold;
+		text-align:center;
+	}
+	#plan-pagos-table .bordeable>tr{
+		border:solid 1px black;
+	}
+</style>
 <!-- page content -->
 <div class="right_col" role="main">
 	<div class="container">
-		PLAN DE PAGOS <?= $_GET['id'] ?>
+		<table class="table table-bordered" id="plan-pagos-table">
+			<thead>
+				<caption><h3 class="text-center">PLAN DE PAGOS</h3></caption>
+				<tr>
+					<th colspan="3">VALOR TOTAL DEL CONTRATO $</th>
+					<th>Valor</th>
+					<th>Fecha</th>
+					<th>Nº de Abono</th>
+				</tr>				
+			</thead>
+			<tbody>
+				<tr>
+					<td>15% del VALOR TOTAL<br>$15.000.000,00</td>
+					<td>
+						<table>
+							<tbody>
+								<tr><td>SEPARACIÓN <br>300.000</td></tr>
+								<tr><td>Saldo del 15%<br>$1.750.000,00</td></tr>
+							</tbody>
+						</table>
+					</td>
+					<td>
+						<table>
+							<tbody class="bordeable">
+								<tr>
+									<td>Cualquier Valor</td>
+								</tr>
+								<tr>
+									<td>Faltante 2 días despúes</td>
+								</tr>
+								<tr>
+									<td>8 días despúes de<br> firma de contrato<br> o separación</td>
+								</tr>
+							</tbody>
+						</table>
+					</td>
+					<td>
+						<table>
+							<tbody class="bordeable">
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+							</tbody>
+						</table>
+					</td>
+					<td>
+						<table>
+							<tbody class="bordeable">
+								<tr>
+									<td>0</td>
+								</tr>
+								<tr>
+									<td>0</td>
+								</tr>
+								<tr>
+									<td>0</td>
+								</tr>
+							</tbody>
+						</table>
+					</td>
+					<td>
+						<table>
+							<tbody class="bordeable">
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+							</tbody>
+						</table>
+					</td>					
+				</tr>								
+			</tbody>
+			<!-- SEGUNDO BODY-->
+			<tbody>
+				<tr>
+					<td colspan="2">
+						85% DEL VALOR TOTAL <br>
+						$
+					</td>
+					<td>
+						Minimo 4 abonos,<br>divididos en valores<br>y tiempos iguales
+					</td>
+					<td>
+						<table>
+							<tbody class="bordeable">
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>								
+							</tbody>
+						</table>
+					</td>
+					<td>
+						<table>
+							<tbody class="bordeable">
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>								
+							</tbody>
+						</table>
+					</td>
+					<td>
+						<table>
+							<tbody class="bordeable">
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+								</tr>								
+							</tbody>
+						</table>
+					</td>					
+				</tr>								
+			</tbody>			
+		</table>
 	</div>
 </div>
